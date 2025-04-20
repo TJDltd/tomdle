f0fc806c8e2b5237e3b36eebc8c3950b485d6e68
Tom Davey <tomjackdavey@hotmail.com>
2025-04-20 21:45:03 +0100

    Added core Tomdle logic

---
+++ src/tomdle.py
@@ -0,0 +1,62 @@
+from tomdle_base import GuessResult, LetterResult, TomdleBase, WordResult
+
+
+class Tomdle(TomdleBase):
+    """Tomdle game implementation.
+
+    Stores game state and allows guessing target words.
+    """
+
+    def __init__(self, word_count: int = 1, word_length: int = 5) -> None:
+        self._guesses = []
+        self._words = []
+        self._word_count = word_count
+        self._word_length = word_length
+
+        self._generate_words(word_count, word_length)
+
+    def _generate_words(self, count: int, length: int) -> None:
+        self._words = [self._generate_word(length) for _ in range(count)]
+
+    def _generate_word(self, _: int) -> str:
+        # Placeholder for word generation logic
+        return "apple"
+
+    def _compare_words(self, guess: str, target: str) -> WordResult:
+        if len(guess) != len(target):
+            m = "Guess and target words must be of the same length."
+            raise ValueError(m)
+
+        letter_results = []
+        for idx, letter in enumerate(guess):
+            if letter == target[idx]:
+                letter_results.append(LetterResult.CORRECT)
+            elif letter in target:
+                letter_results.append(LetterResult.MISPLACED)
+            else:
+                letter_results.append(LetterResult.INCORRECT)
+
+        return WordResult(letter_results=letter_results)
+
+    def add_guess(self, guess: str) -> GuessResult:
+        """Add a guess to the game and return the result.
+
+        Args:
+            guess (str): The guessed word.
+
+        Returns:
+            GuessResult: The result of the guess.
+
+        """
+        if len(guess) != self._word_length:
+            m = f"Guess must be {self._word_length} letters long."
+            raise ValueError(m)
+        if not guess.isalpha():
+            m = "Guess must only contain letters."
+            raise ValueError(m)
+
+        word_results = []
+        for idx, _ in enumerate(self._words):
+            word_results.append(self._compare_words(guess, self._words[idx]))
+
+        return GuessResult(word_results=word_results)
--- src/tomdle_base.py
+++ src/tomdle_base.py
@@ -1,21 +1,22 @@
 import abc
 import enum
+
 import pydantic
+
 
 class LetterResult(enum.Enum):
     CORRECT = "correct"
     INCORRECT = "incorrect"
     MISPLACED = "misplaced"
-    
+
+class WordResult(pydantic.BaseModel):
+    letter_results: list[LetterResult]
 
 class GuessResult(pydantic.BaseModel):
-    letter_results: list[LetterResult]
+    word_results: list[WordResult]
 
 
 class TomdleBase(abc.ABC):
-    def __init__(self):
-        self._guesses = []
-
     @abc.abstractmethod
     def add_guess(self, guess: str) -> GuessResult:
         pass
